e78e0622b7afc06dd809e4394c83251e67a5214b
Fabrizio <fabriziocaricato1@gmail.com>
2025-10-18 20:38:59 -0300

    implementación de ruta protegida en routerapp

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -3,13 +3,17 @@ import Acceso from "../views/Acceso";
 import Home from "../views/Home";
 import PanelPrecios from "../views/PanelPrecios";
 import Restablecer from "../views/Restablecer"
+import ProtectedRoute from "../components/ProtectedRoute";
 
 const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Acceso />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>} />
         <Route path="/panel" element={<PanelPrecios />} />
         <Route path="/restablecer-contraseña" element={<Restablecer />} />
       </Routes>
